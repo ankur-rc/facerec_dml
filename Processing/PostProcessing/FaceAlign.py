bc45773a4dc0c8db773c04eea2e75c38449ca83d
Ankur Roy Chowdhury <ankurrc@tamu.edu>
2018-06-21 17:23:55 -0400

    Implemented face alignment class.

--- Processing/PostProcessing/FaceAlign.py
+++ Processing/PostProcessing/FaceAlign.py
@@ -4,12 +4,14 @@ import cv2
 from LandmarkDetector import LandmarkDetector
 import numpy as np
 import imutils
+import os
 
 """
+Inspired from 'Face Alignment with OpenCV and Python'
 https://www.pyimagesearch.com/2017/05/22/face-alignment-with-opencv-and-python/
 """
 
-LANDMARK_INDEX = {
+FACE_LANDMARK_INDICES = {
     "reye_right": 0,
     "reye_left": 1,
     "leye_right": 3,
@@ -17,80 +19,160 @@ LANDMARK_INDEX = {
     "nose": 4
 }
 
+
+class FaceAlign():
+    """
+    Align a face by performing affine transformations.
+    """
+
+    def __init__(self, left_eye_offset=(0.35, 0.35), final_width=256, final_height=None):
+        """
+        Instantiate a 'FaceAlign' class
+
+        :param left_eye_offset: the amount by which to anchor the left eye center
+        :type left_eye_offset: 2-tuple
+        :param final_width: the width in pixels of the aligned image
+        :type final_width: int
+        :param final_height: the height in pixels of the aligned image
+        :type final_height: int
+        """
+
+        self.left_eye_offset = left_eye_offset
+        self.final_width = final_width
+
+        if final_height is None:
+            self.final_height = final_width
+        else:
+            self.final_height = final_height
+
+    def align(self, img, landmarks):
+        """
+        Align the given image according to given face landmarks -
+        1. Eyes are aligned on a horizontal axis
+        2. Face is scaled to keep eye centers on the same offset location w.r.t the face
+        3. Face is centered on the center of mass of the eyes.
+
+        :param img: rgb or gray-scale image/frame
+        :type img: numpy.ndarray
+        :landmarks: the x,y-coordinates of the facial landmarks
+        :type landmarks: numpy.ndarray
+        :return aligned_img: aligned image of final dimensions
+        :rtype: numpy.ndarray
+        """
+
+        # eye centers
+        leye = tuple(np.add(landmarks[FACE_LANDMARK_INDICES["leye_right"]],
+                            landmarks[FACE_LANDMARK_INDICES["leye_left"]])/2)
+
+        reye = tuple(np.add(landmarks[FACE_LANDMARK_INDICES["reye_right"]],
+                            landmarks[FACE_LANDMARK_INDICES["reye_left"]])/2)
+
+        # angle between eye centers
+        dY = reye[1] - leye[1]
+        dX = reye[0] - leye[0]
+        angle = np.degrees(np.arctan2(dY, dX))
+
+        # right eye coordinates as a function of the left eye
+        right_eye_offset_x = 1.0 - self.left_eye_offset[0]
+
+        # scale
+        current_dist = np.sqrt(dY**2+dX**2)
+        desired_dist = (right_eye_offset_x -
+                        self.left_eye_offset[0])*self.final_width
+        scale = desired_dist/current_dist
+
+        # median of eye centers
+        eye_median = ((reye[0]+leye[0])//2, (reye[1] + leye[1])//2)
+
+        # transformation matrix
+        transform_matrix = cv2.getRotationMatrix2D(eye_median, angle, scale)
+
+        # update the matrix's transaltion compopnents
+        tX = self.final_width * 0.5
+        tY = self.final_height * self.left_eye_offset[1]
+        transform_matrix[0, 2] += (tX - eye_median[0])
+        transform_matrix[1, 2] += (tY - eye_median[1])
+
+        # align the face
+        aligned_face = cv2.warpAffine(
+            img, transform_matrix, (self.final_width, self.final_height), flags=cv2.INTER_CUBIC)
+
+        return aligned_face
+
+
 if __name__ == '__main__':
 
-    image = "../../../../datasets/cyber_extruder_ultimate/000024/000002.jpg"
-    rgbImg = cv2.imread(image)
-    grayImg = cv2.cvtColor(rgbImg, cv2.COLOR_BGR2GRAY)
+    dataset_path = '../../../../datasets/cyber_extruder_ultimate'
+    no_face_file = "no_face.txt"
+    multi_face_file = "multi_face.txt"
 
-    # find a face
     detector = dlib.get_frontal_face_detector()
-    faces = detector(grayImg, 0)
-    landmark_detector = LandmarkDetector()
+    predictor = LandmarkDetector()
 
-    # get face (only interested if there's one and one only)
-    face = faces[0]
+    for root, dirs, files in os.walk(dataset_path):
+        for direc in dirs:
+            path = dataset_path + os.sep + direc + os.sep + '00000%d.jpg'
+            sequence = cv2.VideoCapture(path)
+            while sequence.isOpened():
+                ret, rgbImg = sequence.read()
+                grayImg = None
 
-    # get the landmarks
-    landmarks = landmark_detector.predict(face, grayImg)
-    # cv2.rectangle(rgbImg, (face.left(), face.top()),
-    #               (face.right(), face.bottom()), (0, 0, 255))
+                if rgbImg is None:
+                    break
+                elif rgbImg.shape[2] == 3:
+                    grayImg = cv2.cvtColor(rgbImg, cv2.COLOR_BGR2GRAY)
+                else:
+                    grayImg = rgbImg
 
-    # eye centers
-    leye = tuple(np.add(landmarks[LANDMARK_INDEX["leye_right"]],
-                        landmarks[LANDMARK_INDEX["leye_left"]])/2)
+                # find a face
+                faces = detector(grayImg, 0)
 
-    reye = tuple(np.add(landmarks[LANDMARK_INDEX["reye_right"]],
-                        landmarks[LANDMARK_INDEX["reye_left"]])/2)
+                if len(faces) >= 1:
 
-    # angle between eye centers
-    dY = reye[1] - leye[1]
-    dX = reye[0] - leye[0]
-    angle = np.degrees(np.arctan2(dY, dX))
+                    if len(faces) > 1:
+                        with open(multi_face_file, 'a') as f:
+                            line = direc + ", " + \
+                                str(int(sequence.get(cv2.CAP_PROP_POS_FRAMES)))+"\n"
+                            f.write(line)
 
-    # right eye coordinates as a function on left eye
-    desired_leye = (0.35, 0.35)
-    desired_reye_x = 1.0 - desired_leye[0]
+                    # get face (only interested if there's one and only one)
+                    face = faces[0]
 
-    # scale
-    desired_width = 256
-    desired_height = desired_width
-    current_dist = np.sqrt(dY**2+dX**2)
-    desired_dist = (desired_reye_x - desired_leye[0])*desired_width
-    scale = desired_dist/current_dist
+                    # get the landmarks
+                    landmarks = predictor.predict(face, grayImg)
 
-    # median between eye centers
-    eye_median = ((reye[0]+leye[0])//2, (reye[1] + leye[1])//2)
+                    # get FaceAlign object
+                    face_align = FaceAlign()
 
-    transform_matrix = cv2.getRotationMatrix2D(eye_median, angle, scale)
+                    # align the face
+                    aligned_face = face_align.align(grayImg, landmarks)
 
-    # update the matrix's transaltion compopnents
-    tX = desired_width * 0.5
-    tY = desired_height * desired_leye[1]
-    transform_matrix[0, 2] += (tX - eye_median[0])
-    transform_matrix[1, 2] += (tY - eye_median[1])
+                    # (x, y, w, h) = face_utils.rect_to_bb(face)
+                    # original_face = imutils.resize(
+                    #     rgbImg[y:y + h, x:x + w], width=256)
 
-    # apply affine transformation
+                    # display the output images
+                    # cv2.imshow("Original", original_face)
+                    # cv2.imshow("Aligned", aligned_face)
 
-    aligned_face = cv2.warpAffine(
-        rgbImg, transform_matrix, (desired_width, desired_height), flags=cv2.INTER_CUBIC)
+                    # cv2.circle(rgbImg, leye, 2, (0, 0, 255), -1)
+                    # cv2.circle(rgbImg, reye, 2, (0, 0, 255), -1)
 
-    (x, y, w, h) = face_utils.rect_to_bb(face)
-    original_face = imutils.resize(rgbImg[y:y + h, x:x + w], width=256)
+                    # cv2.line(rgbImg, reye, leye, (0, 0, 255))
 
-    # display the output images
-    cv2.imshow("Original", original_face)
-    cv2.imshow("Aligned", aligned_face)
+                    # for (x, y) in landmarks:
+                    #     cv2.circle(rgbImg, (x, y), 2, (0, 255, 0), -1)
 
-    # cv2.circle(rgbImg, leye, 2, (0, 0, 255), -1)
-    # cv2.circle(rgbImg, reye, 2, (0, 0, 255), -1)
+                    # cv2.imshow("output", rgbImg)
 
-    # cv2.line(rgbImg, reye, leye, (0, 0, 255))
+                    # if cv2.waitKey(500) & 0xFF == ord('q'):
+                    #     sequence.release()
+                    #     cv2.destroyAllWindows()
+                    #     quit()
 
-    # for (x, y) in landmarks:
-    #     cv2.circle(rgbImg, (x, y), 2, (0, 255, 0), -1)
+                else:
 
-    #cv2.imshow("output", rgbImg)
-
-    if cv2.waitKey() & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
+                    with open(no_face_file, 'a') as f:
+                        line = direc + ", " + \
+                            str(int(sequence.get(cv2.CAP_PROP_POS_FRAMES)))+"\n"
+                        f.write(line)
