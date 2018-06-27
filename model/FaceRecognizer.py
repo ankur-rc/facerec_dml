f5988e5fda7edabfc6910f8b3965f27a58f002ca
Ankur Roy Chowdhury <ankurrc@tamu.edu>
2018-06-27 14:38:41 -0400

    Refactored 'FAceRecognizer' class.

--- model/FaceRecognizer.py
+++ model/FaceRecognizer.py
@@ -1,10 +1,8 @@
 from __future__ import division
 import os
 import cv2
-import pickle
-import numpy as np
 import tqdm
-from multiprocessing import Process, Queue
+import numpy as np
 
 from Dataset import Dataset
 
@@ -45,27 +43,19 @@ class FaceRecognizer():
         """
 
         predictions = []
-        # for i in tqdm.trange(0, len(images)):
-        #     prediction = preds_with_indx(i, images[i])
-        #     predictions.append(prediction)
-        #     i += 1
+        for i in tqdm.trange(0, len(images)):
+            prediction = self.model.predict(images[i])
+            predictions.append(prediction)
+            i += 1
 
-        print predictions
         return predictions
 
     def evaluate(self, predictions, ground_truths):
 
         assert(len(predictions) == len(ground_truths))
 
-        true_positive = 0
-        predictions = sorted(predictions, key=lambda x: x[0])
-
-        for idx, pred in enumerate(predictions):
-            if pred[1][0] == ground_truths[idx]:
-                true_positive += 1
-
-        # true_positive = np.count_nonzero(
-        #     np.equal(ground_truths, np.array(predictions)[:, 0]))
+        true_positive = np.count_nonzero(
+            np.equal(ground_truths, np.array(predictions)[:, 0]))
 
         precision_perc = true_positive/len(predictions)*100
 
@@ -79,47 +69,3 @@ class FaceRecognizer():
     def load(self, name):
 
         self.model.read(name)
-
-
-def predict_one(idx, recognizer_obj, image, queue):
-
-    queue.put((idx, recognizer_obj.model.predict(image)))
-
-
-if __name__ == "__main__":
-
-    dataset = Dataset(
-        "/media/ankurrc/new_volume/softura/facerec/datasets/norm_cyber_extruder_ultimate")
-
-    recognizer_model = FaceRecognizer()
-
-    output = Queue()
-
-    X_train, y_train = dataset.load_data(is_train=True, fold=1)
-    print "Training recognizer (", len(X_train), "samples and", len(
-        np.unique(y_train)), "subjects)..."
-    recognizer_model.train(X_train, y_train)
-    print "completed."
-
-    model_name = os.path.basename(dataset.dataset_path) + "__model.yml"
-    print "Saving recognizer model..."
-    recognizer_model.save(model_name)
-    print "done."
-
-    print "Loading recognizer model..."
-    recognizer_model.load(model_name)
-    print "done."
-    X_test, y_test = dataset.load_data(is_train=False, fold=1)
-
-    print "Predicting on (", len(X_test), "samples)..."
-    # predictions = recognizer_model.predict(X_test)
-    processes = [Process(target=predict_one, args=(idx,
-                                                   recognizer_model, image, output)) for idx, image in enumerate(X_test)]
-    for p in processes:
-        p.start()
-    for p in processes:
-        p.join()
-    predictions = [output.get() for p in processes]
-    print "Done"
-
-    recognizer_model.evaluate(predictions, y_test)
---
+++ model/evaluate.py
@@ -0,0 +1,57 @@
+from __future__ import division
+from multiprocessing import Process, Queue
+import numpy as np
+import tqdm
+
+from Dataset import Dataset
+from FaceRecognizer import FaceRecognizer
+
+
+def predict_one(idx, recognizer_obj, image, queue):
+
+    queue.put((idx, recognizer_obj.model.predict(image)))
+
+
+if __name__ == "__main__":
+
+    dataset = Dataset(
+        "/media/ankurrc/new_volume/softura/facerec/datasets/norm_standard_att")
+
+    recognizer_model = FaceRecognizer()
+
+    output = Queue()
+
+    X_train, y_train = dataset.load_data(is_train=True, fold=1)
+    print "Training recognizer (", len(X_train), "samples and", len(
+        np.unique(y_train)), "subjects)..."
+    recognizer_model.train(X_train, y_train)
+    print "completed."
+
+    X_test, y_test = dataset.load_data(is_train=False, fold=1)
+
+    print "Predicting on (", len(X_test), "samples)..."
+
+    # pbar = tqdm.tqdm(total=len(X_test))
+
+    # predictions = recognizer_model.predict(X_test)
+    processes = [Process(target=predict_one,
+                         args=(idx,
+                               recognizer_model,
+                               image,
+                               output))
+                 for idx, image in enumerate(X_test)]
+
+    for p in processes:
+        p.start()
+
+    predictions = [output.get() for p in processes]
+
+    for p in processes:
+        p.join()
+
+    print "Done"
+
+    predictions = [(x[1][0], x[1][1])
+                   for x in sorted(predictions, key=lambda x: x[0])]
+
+    recognizer_model.evaluate(predictions, y_test)
